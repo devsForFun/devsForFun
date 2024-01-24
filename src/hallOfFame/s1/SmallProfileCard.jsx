0e3db1405326c576eb7270798dc7958aef8c432a
CharanMN7_pc <maverickid247@gmail.com>
2024-01-25 01:35:14 +0530

    Completed designing the SmallProfileCard

--- src/hallOfFame/s1/SmallProfileCard.jsx
+++ src/hallOfFame/s1/SmallProfileCard.jsx
@@ -1,13 +1,35 @@
 import { Link, BrowserRouter } from "react-router-dom";
+import blueImg from "../../s1/assets/images/blue.png";
+import blueImg2 from "../../s1/assets/images/blue-2.png";
+import redImg from "../../s1/assets/images/red.png";
+import "./SmallProfileCard.css";
 
 export const SmallProfileCard = ({ person }) => {
   const url = "/hall-of-fame/s1/" + person.username;
+  let color = person.vibe;
+  let secColor;
+  let img;
+  if (color == "red") {
+    color = "#ea4335";
+    secColor = "#f28b82";
+    img = redImg;
+  } else if (color == "blue") {
+    color = "#4285f4";
+    secColor = "#8ab4f8";
+    if (person.gender == "male") {
+      img = blueImg2;
+    } else {
+      img = blueImg;
+    }
+  }
   return (
-    <div>
-      <h1>SmallProfileCard</h1>
-      <b>Name: </b>
-      <span>{person.name}</span>
-      <Link to={url}>View their work</Link>
+    <div className="s1-spcard" style={{ backgroundColor: color }}>
+      <img src={img} alt="" style={{ backgroundColor: secColor }} />
+      <p>{person.name}</p>
+      <Link to={url}>
+        <span>View their work</span>&nbsp;&nbsp;&nbsp;&nbsp;
+        <i class="fa-solid fa-chevron-right"></i>
+      </Link>
     </div>
   );
 };
