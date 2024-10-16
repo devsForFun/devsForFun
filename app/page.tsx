bc068f2ba720da91a557d1a66f06bd56ba0cac53
Charan <charanxmn@gmail.com>
2024-10-16 22:42:12 +0530

    Updated the root page to redirect to the waitlist page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/waitlist");
+  }, []);
   return <></>;
 }
