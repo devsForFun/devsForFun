89b5707fe6f87d3479ef8eb547eb33756d1bf8dd
Charan <charanxmn@gmail.com>
2024-10-19 20:40:30 +0530

    Updated the s1 root pagr to use the s1-base-layout

--- app/s1/page.tsx
+++ app/s1/page.tsx
@@ -5,19 +5,13 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import DottedSeparator from "@/components/dotted-separator";
 import { Badge } from "@/components/ui/badge";
+import S1BaseLayout from "./s1-base-layout";
 
 const images = ["/red.png", "/blue.png", "/yellow.png", "/green.png"];
 
 const Page: React.FC = () => {
   return (
-    <>
-      <header className="w-full sticky top-0 flex justify-between items-center px-4 py-4 border-b border-dashed bg-background/50 backdrop-blur z-[99]">
-        <h1 className="font-semibold text-3xl">devsForFun</h1>
-        <Button variant="outline" disabled className="rounded-full">
-          <Link href="/s1/about">about</Link>
-        </Button>
-      </header>
-
+    <S1BaseLayout>
       {/* hero section */}
       <div className="flex flex-wrap-reverse justify-center p-8 lg:justify-evenly items-center sm:h-[90vh]">
         <div className="mt-8 w-min">
@@ -210,30 +204,7 @@ const Page: React.FC = () => {
       </div>
 
       <DottedSeparator />
-
-      <footer className="text-center text-xs pt-6 pb-4">
-        <Button variant="link" asChild>
-          <Link href="/">devsForFun</Link>
-        </Button>
-        <p className="text-muted-foreground pb-2">
-          by
-          <span className="text-foreground"> gdscrec, </span>
-          <span className="text-foreground">gdsc.ui, </span>
-          <span className="text-foreground">gdsc.kjc, </span>
-          <span className="text-foreground">gdsc_hull</span>
-        </p>
-        <p className="text-muted-foreground pb-2">
-          design inspired by{" "}
-          <a className="text-foreground" href="https://buildspace.so/">
-            buildspace
-          </a>
-        </p>
-        <p className="text-muted-foreground pb-4">
-          this program is not endorsed by Google, the GDSC program, or
-          buiildspace in any way.
-        </p>
-      </footer>
-    </>
+    </S1BaseLayout>
   );
 };
 
