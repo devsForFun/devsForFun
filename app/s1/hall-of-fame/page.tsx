d726e9b5e176cbc82e05f14e8a8a08d3695bedb5
Charan <charanxmn@gmail.com>
2024-10-23 04:13:38 +0530

    Updated the hall-of-fame page with the finishers.
    - Gets the data from the get-finishers API
    - Displays the data using the s1-finishers-card

--- app/s1/hall-of-fame/page.tsx
+++ app/s1/hall-of-fame/page.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import S1BaseLayout from "../s1-base-layout";
+import DottedSeparator from "@/components/dotted-separator";
+import S1FinisherCard from "@/components/s1-finisher-card";
 
 const Page: React.FC = () => {
+  const [finishers, setFinishers] = useState([]);
+
+  useEffect(() => {
+    const fetchFinishers = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/get-finishers");
+        const data = await response.json();
+        console.log("data:", data);
+        setFinishers(data);
+      } catch (error) {
+        console.error("Error fetching finishers:", error);
+      }
+    };
+
+    fetchFinishers();
+  }, []);
+
   return (
     <S1BaseLayout>
-      <></>
+      <div className="text-center my-8">
+        <h2 className="text-5xl font-extrabold">hall of fame</h2>
+      </div>
+      <div className="grid justify-center grid-cols-1 justify-items-center md:grid-cols-2 gap-8 md:gap-10 md:max-w-[640px] mx-auto mb-8">
+        {finishers.map((person) => (
+          <S1FinisherCard
+            key={person.username}
+            id={person.id}
+            name={person.name}
+            vibe={person.vibe}
+          />
+        ))}
+      </div>
+      <DottedSeparator />
     </S1BaseLayout>
   );
 };
