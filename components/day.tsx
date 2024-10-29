10b1a2704674cf55de55c7b9542afd72be73d955
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2024-10-29 13:30:49 +0530

    removed the if condition and used the tailwind css to alternate the positions

--- components/day.tsx
+++ components/day.tsx
@@ -21,63 +21,35 @@ const stickers = [
 ];
 
 export const Day: React.FC<DayProps> = ({ dayNum, title, content, i }) => {
+  const isEven = i % 2 === 0;
   return (
-    <div className="border-b border-r border-l border-dashed p-4 flex flex-nowrap justify-between items-center">
-      {i % 2 === 0 ? (
-        <>
-          <Image
-            src={stickers[i]}
-            alt={`Sticker for day ${dayNum}`}
-            className={`w-64 transform -rotate-[10deg]`}
-            width={256}
-            height={256}
-          />
-          <div className="flex flex-col items-center gap-4 text-left md:flex-row md:items-start">
-            <div className="text-sm md:text-base">
-              <div className="text-3xl mt-2 mb-4 font-extrabold">
-                day {dayNum}
-              </div>
-              <span id="day-title" className="text-muted-foreground">
-                {title}
-              </span>
+    <div
+      className={`border-b border-r border-l border-dashed p-4 flex md:flex-row ${
+        isEven ? "" : "md:flex-row-reverse"
+      } justify-between items-center`}
+    >
+      <Image
+        src={stickers[i]}
+        alt={`Sticker for day ${dayNum}`}
+        className="w-64 transform -rotate-[10deg]"
+        width={256}
+        height={256}
+      />
+      <div className="flex flex-col items-center gap-4 text-left md:flex-row md:items-start">
+        <div className="text-sm md:text-base">
+          <div className="text-3xl mt-2 mb-4 font-extrabold">day {dayNum}</div>
+          <span id="day-title" className="text-muted-foreground">
+            {title}
+          </span>
+          <br />
+          {content.map((line, index) => (
+            <span key={index}>
+              <span className="pb-2 text-muted-foreground">{line}</span>
               <br />
-              {content.map((line, index) => (
-                <span key={index}>
-                  <span className="pb-2 text-muted-foreground">{line}</span>
-                  <br />
-                </span>
-              ))}
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="flex flex-col items-center gap-4 text-left md:flex-row md:items-start">
-            <div className="text-sm md:text-base">
-              <div className="text-3xl mt-2 mb-4 font-extrabold">
-                day {dayNum}
-              </div>
-              <span id="day-title" className="text-muted-foreground">
-                {title}
-              </span>
-              <br />
-              {content.map((line, index) => (
-                <span key={index}>
-                  <span className="pb-2 text-muted-foreground">{line}</span>
-                  <br />
-                </span>
-              ))}
-            </div>
-          </div>
-          <Image
-            src={stickers[i]}
-            alt={`Sticker for day ${dayNum}`}
-            className={`w-64 transform rotate-[10deg]`}
-            width={256}
-            height={256}
-          />
-        </>
-      )}
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
